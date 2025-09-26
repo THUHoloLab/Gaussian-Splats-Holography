3c09d7b237122cd8aa1e5619d037be3ca81f7f5c
Hololab <hololab@mail.tsinghua.edu.cn>
2025-09-26 22:07:53 +0800

    Update rendering_single.m

--- codes/rendering_single.m
+++ codes/rendering_single.m
@@ -6,31 +6,32 @@
 clc
 clear
 
-addpath(genpath("optimizers"));
-addpath(genpath("mxsplat"));
-
 %% load an image
-img = single(imread("cameraman.tif"))/255;
+img = single(imread("peppers.png"))/255;
 img = mean(img,3);
-img_GT = img;
+img = imresize(img,[512,512]);
+
+img_GT = dlarray(img,"SSCB");
 
 img_sz = size(img);
 get_GPU = gpuDevice();
 
-num_Gaussian = 10000;
+num_Gaussian = 30000;
 
-GSH_model = dlnetwork([
-                        inputLayer(([1,num_Gaussian]),'UU');
-                        gSplat_Hologram("device", get_GPU,...
-                                        "img_sz", img_sz);
-                    ]);
+layer = [
+    inputLayer(([1,num_Gaussian]),'UU');
+    mxsplat.gSplat_Hologram("device", get_GPU,...
+                    "img_sz", [512,512]);
+];
+
+GSH_model = dlnetwork(layer);
 
 
 %% Main training loop
 iter = 0;
 iter_max = 4800;
-optimizer = optimizer_AdaBelief();
-lr = 0.006;
+optimizer = optimizers.AdaBelief(0.9,0.99);
+lr = 0.01;
 
 while iter < iter_max
     iter = iter + 1;
@@ -41,38 +42,33 @@ while iter < iter_max
     tt = toc;  
     
     % learning for parameters
-    GSH_model.Learnables = optimizer.step(GSH_model.Learnables,... 
-                                          grad,...
-                                          iter,...
-                                          lr);
+    GSH_model = optimizer.step(GSH_model,grad,iter,lr);
 
     fprintf("at %d-iter, 2d gs takes: %4.5f, " + ...
             "loss: %4.4f, remain gs: %d, \n",iter, tt, loss, num_Gaussian);
 
-    %Adaptive Gaussian density
-    if mod(iter,10) == 0
-        [GSH_model, optimizer, num_Gaussian] = ...
-                            adaptive_Gaussian(GSH_model,...
-                                              optimizer,...
-                                              grad);
-    end
+    % %Adaptive Gaussian density
+    % if mod(iter,10) == 0
+    %     [GSH_model, optimizer, num_Gaussian] = ...
+    %                         adaptive_Gaussian(GSH_model,...
+    %                                           optimizer,...
+    %                                           grad);
+    % end
     
-    if mod(iter,5) == 1
+    if mod(iter,50) == 1
         figure(122);
         imshow(img_out{1},[]);
         drawnow;
     end
 
     % learning rate decay
-    if mod(iter,120) == 0 
-        lr = max(lr * 0.7,0.0001);
+    if (mod(iter,120) == 0) && (iter > 400)
+        lr = max(lr * 0.5,0.0001);
     end
 end
 
 save('results/test_non.mat','GSH_model','img_out');
 
-rmpath(genpath("optimizers"));
-rmpath(genpath("gsplat"));
 reset(gpuDevice());
 
 
@@ -80,7 +76,7 @@ reset(gpuDevice());
 function [loss,grad,img_out] = model_loss(model,img_GT)
     canvas = model.forward(dlarray(1,'U'));
 
-    loss = loss_fun.l2_loss(canvas, img_GT,'mean');
+    loss = l2loss(canvas, img_GT);
 
     grad = dlgradient(loss, model.Learnables);
 
